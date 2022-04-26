778428135c9f8d4d5f65eda5c9281c01d11f3bfe
David Register <dmregister1@gmail.com>
2022-04-26 10:29:17 -0400

    fix: use the default export from jest-environment-node

--- jest-environment-node.js
+++ jest-environment-node.js
@@ -1,4 +1,4 @@
-const NodeEnvironment = require('jest-environment-node');
+const NodeEnvironment = require('jest-environment-node').default;
 const { PollyEnvironmentFactory } = require('./lib/jest-environment-polly');
 
 class PollyEnvironmentNode extends PollyEnvironmentFactory(NodeEnvironment) {}
