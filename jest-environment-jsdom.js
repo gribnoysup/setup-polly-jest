558912c072f857c94e098723a27780bf7f8f3c76
Sergey Petushkov <petushkov.sergey@gmail.com>
2022-05-28 14:36:55 +0200

    chore: Make sure jest intermals match the jest version we are testing

--- jest-environment-jsdom.js
+++ jest-environment-jsdom.js
@@ -1,4 +1,8 @@
-const JSDOMEnvironment = require('jest-environment-jsdom');
+let JSDOMEnvironment = require('jest-environment-jsdom');
+JSDOMEnvironment =
+  JSDOMEnvironment.default ||
+  JSDOMEnvironment.TestEnvironment ||
+  JSDOMEnvironment;
 const { PollyEnvironmentFactory } = require('./lib/jest-environment-polly');
 
 class PollyEnvironmentJSDOM extends PollyEnvironmentFactory(JSDOMEnvironment) {}
