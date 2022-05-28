13edcfff7d3f8e6f47955beb04b8107679404a51
Sergey Petushkov <petushkov.sergey@gmail.com>
2022-05-28 13:43:39 +0200

    chore: Disable compression

--- test/getPost.js
+++ test/getPost.js
@@ -2,7 +2,8 @@ const fetch = require('node-fetch');
 
 const getPost = async id => {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    { compress: false }
   );
   const json = await response.json();
   return Object.assign(json, { ok: response.ok, status: response.status });
